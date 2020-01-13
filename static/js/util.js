b6d6e36b78f911781964238126c049564f6718de
elkip <mitchiscool51@gmail.com>
2020-01-12 22:45:45 -0500

    Content changes and pictures

--- static/js/util.js
+++ static/js/util.js
@@ -1,6 +1,6 @@
 function hideElement(e_id) {
     var x = document.getElementById(e_id);
-    var btn = document.getElementById(this.id);
+    var btn = document.getElementById("btn");
     if(x.style.display == "none") {
         x.style.display = "block";
         btn.innerText="Hide Resume";
