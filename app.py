d14e80ab53f348a91de295ebdb1f4e04b2883387
elkip <mitchiscool51@gmail.com>
2020-01-23 23:02:59 -0500

    Added a contact page to send an email directly to me

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, flash, url_for
+from flask_mail import Mail, Message
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+from forms import ContactForm
+import re
+import os
+
+# RECAPTCHA_PUBLIC_KEY = '6LeYIbsSAAAAACRPIllxA7wvXjIE411PfdB2gt2J'
+# RECAPTCHA_PRIVATE_KEY = '6LeYIbsSAAAAAJezaIq3Ft_hSTo0YtyeFG-JgRtu'
 
 app = Flask(__name__)
+## remember not to upload these to github!!
+app.config['SECRET_KEY'] = '*******************'
+# app.config['SQLALCHEMY_DATABASE_URL'] = 'mysql+pymysql://flask_mail:@localhost/flask_app_db'
+# app.config['SQLALCHEMY_ECHO'] = True
+#
+# db = SQLAlchemy(app)
+
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": '*********************',
+    "MAIL_PASSWORD": '**********************'
+}
+
+app.config.update(mail_settings)
+mail = Mail(app)
 
 
 @app.route('/')
@@ -23,10 +50,32 @@ def blog():
     return render_template('blog.html')
 
 
-@app.route('/test')
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if request.method == "POST":
+        if not re.search(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", form.email.data):
+            flash('You must enter a valid email')
+            return redirect(url_for('contact'))
+        elif not form.validate():
+            flash("Character limit Exceeded")
+        else:
+            with app.app_context():
+                msg = Message(subject="New message from: " + form.name.data,
+                              sender=app.config.get("MAIL_USERNAME"),
+                              recipients=["************@gmail.com"],
+                              body="Message From: " + form.name.data + " \nReply Email: " + form.email.data +
+                                   "\nMessage: " + form.message.data)
+                mail.send(msg)
+            return render_template('test.html', title="Message Sent", message=form.message.data)
+        # return redirect(url_for('test'))
+    return render_template('contact.html', form=form)
+
+
+@app.route('/test', methods=['GET', 'POST'])
 def test():
-    msg = 'Congratulations. You have discovered the secret page.'
-    return render_template('test.html', title="Test", message=msg)
+    msg = 'Success'
+    return render_template('test.html', title="Message Sent", message=msg)
 
 
 if __name__ == '__main__':
